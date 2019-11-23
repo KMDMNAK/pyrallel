3cda87a5fe56bd33d7156af9cbe35dcea128c566
KMDMNAK <kmdmuneaki@gmail.com>
2019-11-23 15:35:39 +0900

    update

--- exep.py
+++ exep.py
@@ -16,6 +16,7 @@ class A:
         self.states = states("a")
 
     def execute(self):
+        print(self.__funcs__)
         for func in self.__funcs__:
             t = threading.Thread(target=func, args=(self.states,))
             t.start()
@@ -24,7 +25,9 @@ class A:
         self.states.a = 1000
 
     def dec(self, func):
+        print(func)
         self.__funcs__.append(func)
+        print(self.__funcs__)
         return func
 
 
@@ -41,7 +44,7 @@ class B:
     def __init__(self):
         setattr(self, "a", 10)
         setattr(self, "b", getattr(self, "a"))
-        
+
     def get(self):
         print(self.a)
         print(self.b)
@@ -51,8 +54,27 @@ class B:
         print(self.b)
 
 
+class C:
+    """
+        property x,yを監視
+    """
+
+    def __init__(self):
+        self._x = []
+        self._y = 0
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+
 if __name__ == "__main__":
-    AC = A()
-    setattr(AC.states,"a",532545)
+    """
+        state自体を渡せば,引数をreactiveに渡せる.
+    """
+    setattr(AC.states, "a", 532545)
     AC.execute()
-    
